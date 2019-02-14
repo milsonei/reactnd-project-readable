4a27ea185184508d758e955b7ee0987fc9ddb2b2
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-14 20:36:20 -0200

    Ajustes no middleware de cache

--- frontend/src/middleware/cache.js
+++ frontend/src/middleware/cache.js
@@ -2,17 +2,19 @@ import Utilities from '../utils/Utilities';
 const cache = ({ getState }) => (next) => (action) => {
     const returnValue = next(action)
     /** Get state after update */
-    const store = getState();
-    console.log(store.authors)
+    const state = getState();
     /** Stores simplified author data to localstorage repository  */
-    if (store.authors &&  store.authors.addToCache){
-        const authors = store.authors;
-        authors.addToCache = false        
+    if (state.authors &&  state.authors.addToCache){
+        const authors = Object.assign({},
+            {   
+                ...state.authors,
+                addToCache: false
+            })     
         Utilities.saveToLocalStorage('authors', authors)
     }
     /** Stores the name of the field responsible for ordering posts in the feed */
-    if (action.type === "SORT_FEED" && store.sort) {        
-        Utilities.saveToLocalStorage('sort', store.sort.field)
+    if (action.type === "SORT_FEED" && state.sort) {        
+        Utilities.saveToLocalStorage('sort', state.sort.field)
     }
     return returnValue
 }
