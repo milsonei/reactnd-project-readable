0702be1bf7501224c5f389ec33fc531fb2c19877
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-14 20:51:35 -0200

    Implementação do criador de ações das postagens

--- frontend/src/actions/posts.js
+++ frontend/src/actions/posts.js
@@ -1,7 +1,12 @@
 import postApi from '../utils/PostApi'
 import Post from '../model/Post'
-import { showLoading, hideLoading } from 'react-redux-loading'
-import DialogUtil from '../utils/DialogUtil'
+import {
+    showLoading,
+    hideLoading
+} from 'react-redux-loading'
+import {
+    showError
+} from '../actions/error'
 export const RECEIVE_POSTS = 'RECEIVE_POSTS'
 export const ADD_POST = 'ADD_POST'
 export const EDIT_POST = 'EDIT_POST'
@@ -9,14 +14,23 @@ export const SET_VOTE_POST = 'SET_VOTE_POST'
 export const UP_VOTE_POST = 'UP_VOTE_POST'
 export const DOWN_VOTE_POST = 'DOWN_VOTE_POST'
 
-export function receivePosts(posts){
+/**
+ * Action responsible for receiving a collection of posts
+ * @param {string} posts Collection of posts
+ */
+export function receivePosts(posts) {
     return {
         type: RECEIVE_POSTS,
         posts: posts
     }
 }
-
-function editPost(id, title, body){
+/**
+ * Action responsible for changing post details
+ * @param {string} id The post id
+ * @param {string} title The title of the post
+ * @param {string} body The body of the post
+ */
+function editPost(id, title, body) {
     return {
         type: EDIT_POST,
         id,
@@ -25,95 +39,126 @@ function editPost(id, title, body){
     }
 }
 
-function addPost(post){
+/**
+ * Action responsible for adding a post
+ * @param {any} post The post
+ */
+function addPost(post) {
     return {
         type: ADD_POST,
         post
     }
 }
-
-function upVotePost({ id }){
+/**
+ * Uptimist update - action responsible for increase the vote score before sending to database 
+ * @param {string} id The post id
+ */
+function upVotePost(id) {
     return {
         type: UP_VOTE_POST,
-        id        
+        id
     }
 }
-
-function downVotePost({ id }){
+/**
+ * Uptimist update - action responsible for descreasing the vote score before sending to database 
+ * @param {string} id The post id
+ */
+function downVotePost(id) {
     return {
         type: DOWN_VOTE_POST,
         id
     }
 }
-
-function setVotePost({ id, voteScore }){
+/**
+ * Action responsible for confirming the vote score after saving it in the database. In a multi-user 
+ * environment, the vote may change before sending.
+ * @param {string} id post id
+ * @param {number} voteScore vote score
+ */
+function setVotePost(id, voteScore) {
     return {
         type: SET_VOTE_POST,
         id,
         voteScore
     }
 }
-
-export function handleEditPost(id, title, body){
-    return async (dispatch) => {       
-        dispatch(showLoading)      
-        try {           
-            await postApi.edit(id, title, body);         
-            dispatch(editPost(id, title, body));   
+/**
+ * The handler responsible for sending changed post details
+ * @param {string} id The id of the post
+ * @param {string} title The title of the post
+ * @param {string} body the body of the post
+ */
+export function handleEditPost(id, title, body) {
+    return async (dispatch) => {
+        dispatch(showLoading)
+        try {
+            await postApi.edit(id, title, body)
+            dispatch(editPost(id, title, body))
+        } catch (e) {
+            dispatch(showError('Edit post', 'The was an error editing post. Try again.'));
         }
-        catch (e) {            
-            DialogUtil.showErrorNotification('Edit post', 'The was an error editing post. Try again.');
-        }
-        return dispatch(hideLoading);    
+        return dispatch(hideLoading)
     }
 }
-
-export function handleAddPost(title, body, author, category){
-    return async (dispatch) => {       
+/**
+ * The async handler responsible for sending new post
+ * @param {string} id The id of the post
+ * @param {string} title The title of the post
+ * @param {string} body the body of the post
+ * @param {string} category the category of the post
+ */
+export function handleAddPost(title, body, author, category) {
+    return async (dispatch) => {
         dispatch(showLoading)
 
         const newPost = new Post(title, body, author, category)
 
         try {
-            const res = await postApi.add(newPost);
+            const res = await postApi.add(newPost)
             const post = res.data;
-            dispatch(addPost(post));
+            dispatch(addPost(post))            
+        } catch (e) {
+            dispatch(showError('New post', 'The was an error adding new post. Try again.'));
         }
-        catch (e) {
-            DialogUtil.showErrorNotification('New post', 'The was an error adding new post. Try again.');
-        }
-        return dispatch(hideLoading);    
+        return dispatch(hideLoading)
     }
 }
 
-export function handleUpVotePost(id){
-    return async (dispatch) => {     
+/**
+ * The async handler responsible for increase the vote score
+ * @param {string} id The post id
+ */
+export function handleUpVotePost(id) {
+    return async (dispatch) => {
         dispatch(showLoading)
-        try {         
-            dispatch(upVotePost(id));   
-            const res = await postApi.upVote(id);    
-            dispatch(setVotePost(id, res.voteScore));
-        }
-        catch (e) {
+        try {
+            dispatch(upVotePost(id));
+            const res = await postApi.upVote(id)
+            const post = res.data
+            dispatch(setVotePost(id, post.voteScore))
+        } catch (e) {
+            dispatch(showError('Up vote', 'The was an error up voting the post. Try again. Detail:' + e.message))
             dispatch(downVotePost(id));
-            DialogUtil.showErrorNotification('Up vote', 'The was an error up voting the post. Try again.');
         }
         return dispatch(hideLoading);
     }
 }
-
-export function handleDownVotePost(id){
-    return async (dispatch) => {     
+/**
+ * The async handler responsible for decreasing the vote score
+ * @param {string} id The post id
+ */
+export function handleDownVotePost(id) {
+    return async (dispatch) => {
         dispatch(showLoading)
         try {
-            dispatch(downVotePost(id));   
-            const res = await postApi.downVote(id);
-            const post = res.data;
-            dispatch(setVotePost(post.id, post.voteScore));
-        }
-        catch (e) {
-            dispatch(upVotePost(id));   
-            DialogUtil.showErrorNotification('Down vote', 'The was an error down voting the post. Try again.');
+            dispatch(downVotePost(id));
+            const res = await postApi.downVote(id)
+            const post = res.data
+            dispatch(setVotePost(post.id, post.voteScore))
+        } catch (e) {
+            dispatch(showError('Up vote', 'The was an error up voting the post. Try again. Detail:' + e.message))
+            dispatch(upVotePost(id))
+
         }
         return dispatch(hideLoading);
     }
