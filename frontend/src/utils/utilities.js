89856a572d296f474b0b5769d14e3932a2cc9416
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-14 20:32:18 -0200

    Classe de rotinas de apoio

--- frontend/src/utils/utilities.js
+++ frontend/src/utils/utilities.js
@@ -1,35 +1,68 @@
-const utilities = {  
+ class Utilities { 
+   /**
+     * Get sort condition to sort function
+     * @param {any} firstData First data to compare
+     * @param {any} secondData Second data to compare
+     * @param {string} direction asc or desc
+     * @return {number} 1 to ascending sort, -1 to descending sort and 0 to none
+     */
+    static getSortCondition (firstData, secondData, direction) {    
+        const sortKey = direction === "asc" ? 1 : -1   
+        if (firstData > secondData) {
+            return 1*sortKey;
+          }
+          if (firstData < secondData) {
+            return -1*sortKey;
+          }
+          // a must be equal to b
+          return 0;
+    }
      /**
      * Normalize author array into object and add random avatar icon
-     * @param {array} arr Author array
+     * @param {array} newAuthors New authors array
+     * @param {any} oldAuthors Existent authors array
      * @return {object} Normalized object
      */
-    createAuthors :  (arr) => {
-        let authors = {}
-        arr.forEach(author => authors[author] = {name: author, avatar: utilities.generateRandomAvatar()});
+    static createAuthors(newAuthors, oldAuthors){                 
+        let authors 
+        if (oldAuthors)           {
+            authors = {...oldAuthors} 
+        }else{
+            authors = {}
+        }        
+    
+        const existentKeys = Object.keys(authors)        
+        const exists = (author) => existentKeys.filter(key => key === author).length > 0
+        let filteredAuthors = newAuthors.filter(author => exists(author) === false)
+        if (filteredAuthors.length === 0) return {}
+        filteredAuthors.forEach(author => authors[author] = {name: author, avatar: Utilities.generateRandomAvatar()})   
         return authors
-    },
+    }    
     /**
      * Generate a new identify
      */
-    generateUID : () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
-
+    static generateUID(){
+        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+    }
     /**
      * Generate a new timestamp
      */
-    generateTimestamp : () => Date.now(),
+    static generateTimestamp() {
+        return Date.now()
+    }
 
     /**
      * Generates a random number between a minimum value and a maximum value
      * @param {integer} min Mininum value
      * @param {integer} max Maximum value
      */
-    generateRandomNumber : (min, max) => Math.floor(Math.random() * (max - min) + min),
-
+    static generateRandomNumber(min, max){
+        return Math.floor(Math.random() * (max - min) + min)
+    }
     /**
      * Generate a random avatar from https://getavataaars.com
      */
-    generateRandomAvatar : () => {
+    static generateRandomAvatar() {
         const parameters = {
             topType: ["NoHair", "Eyepatch", "Hat", "Hijab", "Turban", "WinterHat1", "WinterHat2", "WinterHat3", "WinterHat4", "LongHairBigHair", "LongHairBob", "LongHairBun", "LongHairCurly", "LongHairCurvy", "LongHairDreads", "LongHairFrida", "LongHairFro", "LongHairFroBand", "LongHairNotTooLong", "LongHairShavedSides", "LongHairMiaWallace", "LongHairStraight", "LongHairStraight2", "LongHairStraightStrand", "ShortHairDreads01", "ShortHairDreads02", "ShortHairFrizzle", "ShortHairShaggyMullet", "ShortHairShortCurly", "ShortHairShortFlat", "ShortHairShortRound", "ShortHairShortWaved", "ShortHairSides", "ShortHairTheCaesar", "ShortHairTheCaesarSidePart"],
             accessoriesType: ["Blank", "Kurt", "Prescription01", "Prescription02", "Round", "Sunglasses", "Wayfarers"],
@@ -42,16 +75,31 @@ const utilities = {
             mouthType: ["Concerned", "Default", "Disbelief", "Eating", "Grimace", "Sad", "ScreamOpen", "Serious", "Smile", "Tongue", "Twinkle", "Vomit"],
             skinColor: ["Tanned", "Yellow", "Pale", "Light", "Brown", "DarkBrown", "Black"]
         }
-
+        
         const urlParams = Object.keys(parameters).map(key => {
             const arr = parameters[key]
-            const index = this.generateRandomNumber(0, arr.length)
+            const index = Utilities.generateRandomNumber(0, arr.length)
             const value = arr[index]
             return `${key}=${value}`
         }).join('&')
 
         return `https://avataaars.io/?${urlParams}`
     }
+    /**
+     * Save item to html5 local storage
+     * @param {string} key 
+     * @param {any} value 
+     */
+    static saveToLocalStorage(key, value) {    
+        localStorage.setItem(key, JSON.stringify(value))
+    }
+    /**
+     * Get item from html5 local storage
+     * @param {string} key 
+     */
+    static getFromLocalStorage(key) {    
+        return JSON.parse(localStorage.getItem(key))
+    }
 }
 
-export default utilities
+export default Utilities
