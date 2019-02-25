f0da3497269359653b5aa33b5577c5a7d5c7c1df
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-24 23:17:31 -0300

    Component responsible for rendering a page that lists all posts based on the MyPost custom component, which shows the title, body limited to 60 characters, author, time, comment counter, and post category.

--- frontend/src/components/FeedPage.js
+++ frontend/src/components/FeedPage.js
@@ -1,46 +1,101 @@
 import React,  { Component } from 'react'
 import { connect } from 'react-redux'
-import AuthoralPost from '../components/AuthoralPost'
+import MyPost from './MyPost'
+import SortSelector from './SortSelector';
 import Utilities from '../utils/Utilities'
+import SearchInput from './SearchInput';
+import { Col, Row } from 'antd';
+import PropTypes from 'prop-types';
+/**
+ * Component responsible for rendering a page that lists all posts based on the MyPost custom component, 
+ * which shows the title, body limited to 60 characters, author, time, comment counter, and post category.
+ */
 class FeedPage extends Component {    
         /**
      * Show a notification after render page
      */
-    componentDidUpdate(){
-       
+    componentDidMount(){
+        document.title = this.props.title;
     }
 
-    render(){ 
+    render(){         
+        const { inSearchMode, title, postIds } = this.props
+        const showSort =  postIds.length > 1
         return (
             <div>
-                <h2 className='feed-title center'>{this.props.title}</h2>
-                <ul className='dashboard-list'>
-                    {
-                        this.props.postIds.map((id) => (
-                            <li key={id}>
-                                <AuthoralPost id={id}/>                               
-                            </li>
-                        ))
-                    }
-                </ul>
+                <div className="my-box widget-box feed">
+                    <div className="header">
+                        {title}
+                    </div>   
+                    {(showSort || inSearchMode) && (    
+                    <div className="widget-body">
+                    <Row>
+                        <Col span={12}>
+                         <SortSelector/>
+                        </Col>
+                        <Col span={12}>
+                         <SearchInput/>
+                        </Col>
+                    </Row>
+                       
+                    </div>)}
+                </div>
+                <div>
+                    <ul className='dashboard-list'>
+                        {
+                            postIds.map((id) => (
+                                <li key={id}>
+                                    <MyPost id={id}/>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
             </div>
         )
     }
 }
 
-function mapStateToProps({ posts, sort }, props){
-    let { id } = props.match.params
-    let sortField = "voteScore"    
-    if (sort){
-        sortField = sort.field
-    }   
+function mapStateToProps({ posts, comments, sort, search }, props){
+    const { id } = props.match.params
+    const { field, mode } = sort.posts
     const category = id 
-    const postIds = (category ? Object.keys(posts).filter(key => posts[key].category === category) : Object.keys(posts))
-                    .sort((a,b) => Utilities.getSortCondition(posts[a][sortField],posts[b][sortField], sortField === "voteScore" ? 'desc' : 'asc'))
-    return {
-        title: category ? `Feed of ${category}` : "Feed",        
+    const activeKeys = Utilities.getActiveKeys(posts)
+    let keysInSearch
+    let inSearchMode = false;
+  
+    if (search && search.text){
+        inSearchMode = true
+        /** Search in comments */
+        const keysInComments = Object.keys(comments)
+                                    .filter(key => comments[key].body.toLowerCase().indexOf(search.text.toLowerCase()) > -1)
+                                    .map(key => comments[key].parentId)
+
+        keysInSearch = activeKeys.filter(key => posts[key].category.toLowerCase().indexOf(search.text.toLowerCase()) > -1 ||
+                                                posts[key].title.toLowerCase().indexOf(search.text.toLowerCase()) > -1 ||
+                                                posts[key].body.toLowerCase().indexOf(search.text.toLowerCase()) > -1 ||
+                                                posts[key].author.toLowerCase().indexOf(search.text.toLowerCase()) > -1)
+        keysInSearch = Utilities.distinct([...keysInComments,...keysInSearch])
+    }else{
+        keysInSearch = activeKeys
+
+    }
+        
+    const postIds = (category 
+                    ? keysInSearch.filter(key => posts[key].category === category) 
+                    : keysInSearch)
+                    .sort((a,b) => Utilities.getSortCondition(posts[a][field],posts[b][field], mode))
+    return {       
+        inSearchMode,
+        title: category ? `Feed of ${category}` : "Feed",            
         postIds
     }
+}
+
+FeedPage.propTypes = {
+    inSearchMode: PropTypes.bool,
+    title: PropTypes.string,
+    postIds: PropTypes.array
 }
 
 export default connect(mapStateToProps)(FeedPage)
