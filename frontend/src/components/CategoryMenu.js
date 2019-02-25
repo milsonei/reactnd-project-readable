935ce7a210ac2e6d611ca0bd9b036bb43ba36a76
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-24 23:16:54 -0300

    Stateless component responsible for create a category menu based on Dropdown component from React UI library ANTD

--- frontend/src/components/CategoryMenu.js
+++ frontend/src/components/CategoryMenu.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Menu, Dropdown, Icon } from 'antd';
-export default function CategoryMenu(props){
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types';
+import { enableRedirect } from '../actions/redirect';
+/**
+ * Stateless component responsible for creating a category menu based on the Dropdown component of the React UI antd library
+ * @param {any} props Properties
+ */
+function CategoryMenu(props){
   const { categories } = props
   const menu = (
                 <Menu key="category-menu">
                    <Menu.Item key="all">
-                      <a rel="noopener noreferrer" href={`/`}>All</a>
-                    </Menu.Item>
+                    <div onClick={()=> props.dispatch(enableRedirect("/"))}>All</div>            
+                  </Menu.Item>
                 {(Object.keys(categories).map(category => (
                     <Menu.Item key={category}>
-                      <a rel="noopener noreferrer" href={`/posts/${category}`}>{category}</a>
+                      <div onClick={()=> props.dispatch(enableRedirect(`/posts/${category}`))}>{category}</div>                      
                     </Menu.Item>)))}   
                 </Menu>
               );       
@@ -19,3 +26,15 @@ export default function CategoryMenu(props){
         </Dropdown> 
       )
   }
+
+  function mapStateToProps({ categories }){
+    return {
+      categories: categories || {}
+      }
+  }
+  
+  CategoryMenu.propTypes = {
+    categories: PropTypes.object 
+  }
+
+  export default connect(mapStateToProps)(CategoryMenu)
