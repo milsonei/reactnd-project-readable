5c7764c6793b19bb2291d2bd159bd6806f32c092
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-24 23:21:03 -0300

    Specify how the categories's state changes in response to actions sent to the store

--- frontend/src/reducers/categories.js
+++ frontend/src/reducers/categories.js
@@ -1,5 +1,9 @@
 import { RECEIVE_CATEGORIES } from '../actions/categories'
-
+/**
+ * This reducer specify how the categories's state changes in response to actions sent to the store
+ * @param {any} state 
+ * @param {any} action 
+ */
 export default function categories( state = null, action){
     switch (action.type) {
         case RECEIVE_CATEGORIES:
