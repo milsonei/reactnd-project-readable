aa1bd8a1fd85830f404a830921dc43e7d0ff2186
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-24 23:18:58 -0300

    Initialize store and dispatch, which will be used later.

--- frontend/src/middleware/index.js
+++ frontend/src/middleware/index.js
@@ -3,7 +3,9 @@ import logger from './logger'
 import error from './error'
 import cache from './cache'
 import { applyMiddleware } from 'redux'
-
+/**
+ * Initialize store and dispatch, which will be used later.
+ */
 export default applyMiddleware(
     thunk,
     logger,
