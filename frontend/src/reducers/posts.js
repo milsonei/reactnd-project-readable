b798f8ab5f7731706daead466480a7ec5fa583bd
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-24 23:21:55 -0300

    Specify how the posts's state changes in response to actions sent to the store

--- frontend/src/reducers/posts.js
+++ frontend/src/reducers/posts.js
@@ -4,36 +4,56 @@ import {
     ADD_POST,
     SET_VOTE_POST,
     UP_VOTE_POST,
-    DOWN_VOTE_POST
+    DOWN_VOTE_POST,
+    INCREMENT_COMMENT_COUNTER,
+    DECREMENT_COMMENT_COUNTER,
+    DELETE_POST
 } from '../actions/posts'
 
+import Utilities from '../utils/Utilities';
+/**
+ * This reducer specify how the posts's state changes in response to actions sent to the store
+ * @param {any} state 
+ * @param {any} action 
+ */
 export default function posts(state = {}, action) {
     switch (action.type) {
         case RECEIVE_POSTS:
-            let posts = {}
-            action.posts.forEach(post => posts[post.id] = post)
+            const posts = Utilities.normalize(action.posts)
             return {
                 ...state,
-                ...posts
-            }
+                ...posts,
+            }         
         case EDIT_POST:
             const {
                 id,
                 title,
-                body
+                body,
+                category
             } = action
-            return {
+            return { 
                 ...state,
                 [id]: {
                     ...state[id],
                     title,
-                    body
+                    body,
+                    category
                 }
             }
         case ADD_POST:
+            let newPost = {}
+            newPost[action.post.id] = action.post
+            return {
+                ...state,                
+                ...newPost
+            }
+        case DELETE_POST:
             return {
                 ...state,
-                ...action.post
+                [action.id]: {
+                    ...state[action.id],
+                    deleted: true
+                }
             }
         case SET_VOTE_POST:
             return {
@@ -41,6 +61,22 @@ export default function posts(state = {}, action) {
                 [action.id]: {
                     ...state[action.id],
                     voteScore: action.voteScore
+                }
+            }
+        case INCREMENT_COMMENT_COUNTER:
+            return {
+                ...state,
+                [action.id]: {
+                    ...state[action.id],
+                    commentCount: state[action.id].commentCount + 1
+                }
+            }
+        case DECREMENT_COMMENT_COUNTER:
+            return {
+                ...state,
+                [action.id]: {
+                    ...state[action.id],
+                    commentCount: state[action.id].commentCount - 1
                 }
             }
         case UP_VOTE_POST:
@@ -51,7 +87,7 @@ export default function posts(state = {}, action) {
                     voteScore: state[action.id].voteScore + 1
                 }
             }
-        case DOWN_VOTE_POST:        
+        case DOWN_VOTE_POST:
             return {
                 ...state,
                 [action.id]: {
