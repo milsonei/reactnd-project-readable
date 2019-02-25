510eccce61b6307f0e6167b64dfbf6a495f78481
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-24 23:18:22 -0300

    Middleware responsible for being a common point of capture and storage in the local data storage coming from several actions.

--- frontend/src/middleware/cache.js
+++ frontend/src/middleware/cache.js
@@ -1,21 +1,38 @@
 import Utilities from '../utils/Utilities';
-const cache = ({ getState }) => (next) => (action) => {
+import { SORT } from '../actions/sort';
+import { SET_AUTHED_USER, SET_SIGNOUT } from '../actions/authedUser';
+import { SET_REMEMBER } from '../actions/remember';
+/**
+ * Middleware responsible for being a common point of capture and storage in the local data storage coming from several actions.
+ */
+const cache = () => (next) => (action) => {
     const returnValue = next(action)
-    /** Get state after update */
-    const state = getState();
-    /** Stores simplified author data to localstorage repository  */
-    if (state.authors &&  state.authors.addToCache){
-        const authors = Object.assign({},
-            {   
-                ...state.authors,
-                addToCache: false
-            })     
-        Utilities.saveToLocalStorage('authors', authors)
+    if (action.type === SORT) {   
+        Utilities.saveToLocalStorage(`sort-${action.itemToSort}`, { field: action.field, mode: action.mode })
     }
-    /** Stores the name of the field responsible for ordering posts in the feed */
-    if (action.type === "SORT_FEED" && state.sort) {        
-        Utilities.saveToLocalStorage('sort', state.sort.field)
-    }
+
+    if (action.type === SET_AUTHED_USER) {
+        if (action.id){
+            Utilities.saveToLocalStorage('authed-user', {
+                id: action.id,
+                date: action.date
+            })
+        }
+    } 
+
+    if (action.type === SET_SIGNOUT) {
+        Utilities.saveToLocalStorage('authed-user', {
+            id: '',
+            date: null
+        })    
+    } 
+
+    if (action.type === SET_REMEMBER) {
+        Utilities.saveToLocalStorage('remember', {
+            id: action.id,
+            date: action.date
+        })    
+    } 
     return returnValue
 }
 
