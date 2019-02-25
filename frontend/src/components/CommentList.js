adc5518ed8bf2658b0690ae512b51994e5881d86
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-24 23:17:15 -0300

    Component responsible for create a comment list based on custom component MyComment

--- frontend/src/components/CommentList.js
+++ frontend/src/components/CommentList.js
@@ -1,14 +1,41 @@
-import {Comment, List} from 'antd';
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import MyComment from './MyComment'
+import Utilities from '../utils/Utilities'
+import PropTypes from 'prop-types'
+/***
+ * Component responsible for create a comment list based on custom component MyComment.
+ */
+class CommentList extends Component{
+  render(){
+    const { comments } = this.props
+    let hiddenAttr = {}
+    if (comments.length === 0){
+      hiddenAttr = {style:{display:"none"}}
+    }
+    return (<div {...hiddenAttr} className="my-box widget-box comment no-border-radius"> <ul>
+    {comments.map((id, index) =>(     
+        <li className="content-container" key={`comment-container-${id}`}>
+            <MyComment id={id}/>
+        </li>
+    )
+    )}
+  </ul></div>)
+  }
+}
 
-const CommentList = ({ comments }) => (
-    <List
-      dataSource={comments}
-      header={`${comments.length} ${comments.length > 1 ? 'replies' : 'reply'}`}
-      itemLayout="horizontal"
-      renderItem={props => <Comment {...props} />}
-    />
-  );
+function mapStateToProps({ comments, sort }, { parent }) { 
+  const { field, mode } = sort.comments
+  const activeKeys = Utilities.getActiveKeys(comments)
+  return {
+      comments: activeKeys
+                .filter(key => comments[key].parentId === parent)
+                .sort((a,b) => Utilities.getSortCondition(comments[a][field], comments[b][field], mode))
+  }
+}
 
-  export default CommentList
+CommentList.propTypes = {
+  comments: PropTypes.array
+}
 
+export default connect(mapStateToProps)(CommentList)
