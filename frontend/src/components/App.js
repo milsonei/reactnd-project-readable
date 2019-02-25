b5d6e65bf9f703ab21ee48376ca7f2ffa3e30727
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-24 23:16:29 -0300

    This is the root of app. Contains pages routes.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,56 +2,58 @@ import React, { Component, Fragment } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
-import handleSortFeed from '../actions/sort'
 import FeedPage from './FeedPage'
 import LoadingBar from 'react-redux-loading'
-import NewPostPage from './NewPostPage'
+import PostEditPage from './PostEditPage'
 import Nav from './Nav'
-import PostPage from './PostPage';
-
+import PostViewPage from './PostViewPage'
+import UserEditPage from './UserEditPage'
+import LoginPage from './LoginPage'
+import PropTypes from 'prop-types'
+import '../css/App.css'
+import "antd/dist/antd.css";
 class App extends Component {
   componentDidMount(){
     this.props.dispatch(handleInitialData())
   }
-  handleSortFeed(field){
-    this.props.dispatch(handleSortFeed(field))
-  }
-  render() {    
-      /**
-       * Normalmente seria criado uma div aqui, mas isto adicionaria outro div à DOM, o que não é recomendável.
-       * Então para resolver isso vamos usar Fragment, que permite passar um único filho para o componente router, de React Router, 
-       * mas não vai adicionar outros elementos à DOM
-       */
+
+  render() {
     return (
       <Router>
         <Fragment>
           <LoadingBar/>
-          <div className='container'>
-            <Nav sort={this.props.sort} categories={this.props.categories} onSortFeed={(field) => this.handleSortFeed(field)}/>
+          <Nav/>                  
             { 
               this.props.loading === true
               ? null
               : 
-              <div>
+              <div className='container'>   
                 <Route path='/' exact component={FeedPage}/>
-                <Route path='/post/:id' exact component={PostPage}/>                
-                <Route path='/posts/:id' exact component={FeedPage}/>                
-                <Route path='/new' exact component={NewPostPage}/>
+                <Route path='/signin' exact component={LoginPage}/>    
+                <Route path='/join' exact component={UserEditPage}/>
+                <Route path='/profile' exact component={UserEditPage}/>
+                <Route path='/change-password' exact component={UserEditPage}/>
+                <Route path='/post/:id' exact component={PostViewPage}/>
+                <Route path='/posts/:id' exact component={FeedPage}/>
+                <Route path='/new' exact component={PostEditPage}/>
+                <Route path='/edit/:id' exact component={PostEditPage}/>
               </div>
             }
-          </div>
+          
         </Fragment>
       </Router>
     )
   }
 }
 
-function mapStateToPropos({ authedUser, categories, sort }){
+function mapStateToPropos({ loaded }){
   return {
-    sort,
-    categories,
-    loading: authedUser === null
+    loading: loaded == null
   }
+}
+
+PostEditPage.propTypes = {
+  loading: PropTypes.bool 
 }
 
 export default connect(mapStateToPropos)(App)
