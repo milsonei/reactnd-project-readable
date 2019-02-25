23f39ab1b0780c78300aa263a9ea028135f5ef97
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-24 23:21:14 -0300

    Specify how the comments's state changes in response to actions sent to the store

--- frontend/src/reducers/comments.js
+++ frontend/src/reducers/comments.js
@@ -1,69 +1,83 @@
 import {
     RECEIVE_COMMENTS,
+    EDIT_COMMENT,
     ADD_COMMENT,
     SET_VOTE_COMMENT,
     UP_VOTE_COMMENT,
-    DOWN_VOTE_COMMENT
+    DOWN_VOTE_COMMENT,
+    DELETE_COMMENT,
+    DELETE_ALL_COMMENTS
 } from '../actions/comments'
-
+import Utilities from '../utils/Utilities';
+/**
+ * This reducer specify how the comments's state changes in response to actions sent to the store
+ * @param {any} state 
+ * @param {any} action 
+ */
 export default function comments(state = {}, action) {
-    const {
-        postId,
-        commentId,
-        voteScore
-    } = action;
     switch (action.type) {
         case RECEIVE_COMMENTS:
-            let comments = {}
-            action.comments.forEach(comment => comments[comment.id] = comment)
+            const comments = Utilities.normalize(action.comments)
             return {
                 ...state,
-                [action.postId]: {
-                    ...state[action.postId],
-                    ...comments
+                ...comments
+            }
+        case EDIT_COMMENT:
+            const {
+                id,
+                body
+            } = action
+            return {
+                ...state,
+                [id]: {
+                    ...state[id],
+                    body
                 }
             }
         case ADD_COMMENT:
+            const comment = {}
+            comment[action.comment.id] = action.comment
             return {
                 ...state,
-                [postId]: {
-                    ...state[postId],
-                    [commentId]: action.comment
+                ...comment
+            }
+        case DELETE_COMMENT:
+            return {
+                ...state,
+                [action.id]: {
+                    ...state[action.id],
+                    deleted: true
                 }
-
+            }
+        case DELETE_ALL_COMMENTS:
+            const filtered_keys = Object.keys(state).filter(key => state[key].parentId === action.parentId)
+            const clone = {...state}
+            filtered_keys.forEach(key => clone[key].parentDeleted = true)
+            return {
+               ...clone
             }
         case SET_VOTE_COMMENT:
-           
             return {
                 ...state,
-                [postId]: {
-                    ...state[postId],
-                    [commentId]: {
-                        ...state[postId][commentId],
-                        voteScore
-                    }
+                [action.id]: {
+                    ...state[action.id],
+                    voteScore: action.voteScore
                 }
             }
-        case UP_VOTE_COMMENT:            
+        case UP_VOTE_COMMENT:
             return {
                 ...state,
-                [postId]: {
-                    ...state[postId],
-                    [commentId]: {
-                        ...state[postId][commentId],
-                        voteScore: state[postId][commentId].voteScore++
-                    }
+                [action.id]: {
+                    ...state[action.id],
+                    voteScore: state[action.id].voteScore + 1
                 }
             }
-        case DOWN_VOTE_COMMENT:           
+        case DOWN_VOTE_COMMENT:        
             return {
                 ...state,
-                [postId]: {
-                    ...state[postId],
-                    [commentId]: {
-                        ...state[postId][commentId],
-                        voteScore: state[postId][commentId].voteScore--
-                    }
+                [action.id]: {
+                    ...state[action.id],
+                    voteScore: state[action.id].voteScore - 1
                 }
             }
         default:
