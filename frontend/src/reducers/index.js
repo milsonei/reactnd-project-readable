a9d16ab48ea37f3ae9af904523c906a10f403991
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-24 23:21:40 -0300

    Combine reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,18 +1,30 @@
 import { combineReducers } from 'redux'
 import authedUser from './authedUser'
 import posts from './posts'
-import authors from './authors'
+import users from './users'
 import comments from './comments'
 import categories from './categories'
 import sort from './sort'
+import avatar from './avatar'
+import redirect from './redirect'
+import remember from './remember'
+import success from './success'
+import loaded from './loaded'
+import search from './search'
 import { loadingBarReducer } from 'react-redux-loading'
 
 export default combineReducers({
+    remember,
     authedUser,
     categories,
     posts,
     comments,
-    authors,
+    users,
+    avatar,
     sort,
+    search,
+    redirect,
+    success, 
+    loaded, 
     loadingBar: loadingBarReducer
 })
