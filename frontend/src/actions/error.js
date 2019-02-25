cfe82a7fcefdcbaa68aa7817defed2e874e0a2ab
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-24 23:00:04 -0300

    Action to show an error notification about the previous action having completed with error

--- frontend/src/actions/error.js
+++ frontend/src/actions/error.js
@@ -1,6 +1,10 @@
 export const SHOW_ERROR = 'SHOW_ERROR'
 export const HIDE_ERROR = 'HIDE_ERROR'
-
+/**
+ * Action to show an error notification about the previous action having completed with error
+ * @param {string} title 
+ * @param {message} message 
+ */
 export function showError(title, message){
     return {
         type: SHOW_ERROR,
