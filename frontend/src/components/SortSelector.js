a9c15f769d5ba6c3073cb0337189f340462c653e
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-24 23:18:03 -0300

    Component responsible for creating a list containing fields available for sorting a list of posts or comments. The AND components used on this page are: Form, Select and Tooltip.

--- frontend/src/components/SortSelector.js
+++ frontend/src/components/SortSelector.js
@@ -1,42 +1,88 @@
 import React, {Component} from 'react'
-import { Select, Form, Tooltip } from 'antd';
-class SortMenu extends Component{
-   menuItems = [{id:"voteScore", name:"Vote Score"}, 
-                {id:"timestamp", name:"Creation date"}, 
-                {id: "title", name: "Title"}]  
+import { Form, Select, Tooltip } from 'antd';
+import { connect } from 'react-redux'
+import { handleSetSort } from '../actions/sort';
+import PropTypes from 'prop-types';
+/**
+ * Component responsible for creating a list containing fields available for sorting a list of posts or comments.
+ * The AND components used on this page are: Form, Select and Tooltip.
+ */
+class SortSelector extends Component{
+   menuItems = {
+      "voteScore": {id:"voteScore", name:"Vote Score", mode: "desc"}, 
+      "timestamp": {id:"timestamp", name:"Creation date", mode: "desc"}, 
+      "title": {id: "title", name: "Title", mode: "asc"}
+    }
     /**
      * Call routine parent responsible for sorting the post feed
      * @param {string} field field name for sorting
      */                  
     handleChange(field){         
-      this.props.onSortFeed(field)
+      this.props.dispatch(handleSetSort(field, this.menuItems[field].mode, this.getType()))
+    }
+    /**
+     * Get type of sort - posts or comments
+     */
+    getType(){
+      const { type } = this.props
+      if (type) return type
+      return "posts"
     }
     /**
      * Generate select items
      */
     generateItens() {
-      return this.menuItems.map((item, index) => (
-        <Select.Option key={`item-sort-${index}`} value={item.id}>{item.name}</Select.Option>
+      const type =  this.getType()
+      let keys = Object.keys(this.menuItems)
+      if (type){
+        if (type === 'comments'){
+          keys = keys.filter(key => key !== 'title')
+        }
+      }
+    
+      return keys.map((key, index) => (
+        <Select.Option key={`item-sort-${index}`} value={this.menuItems[key].id}>{this.menuItems[key].name}</Select.Option>
       ))
     }
     
-    render(){  
+    render(){        
       const { getFieldDecorator } = this.props.form;
       const { field } = this.props
-      return  (<Form><Form.Item><Tooltip placement="leftTop" title="Sort by">{
+      const formItemLayout = {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 10 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 14 },
+        },
+      };
+      return  ( <Form id="sortby"><Form.Item  {...formItemLayout} label="Sort by" style={{marginBottom:0}}><Tooltip placement="leftTop" title="Sort by">{
         getFieldDecorator('sort-selector', 
         {          
-          initialValue: field || 'timestamp'
+          initialValue: field
         })(
-          
             <Select key="sort" placeholder="Sort by" className="sort" onChange={(value) => this.handleChange(value)}>
               {this.generateItens()}
             </Select>
-         
       )
       } </Tooltip></Form.Item></Form>)
     }
-  }
+}
 
-  const WrappedSortMenu = Form.create()(SortMenu)
-  export default WrappedSortMenu
+function mapStateToProps({ sort }, { type = 'posts' }){
+  const sortInfo = sort[type]
+  return {
+    field: sortInfo.field || 'voteScore'
+    }
+}
+
+const WrappedSortSelector = Form.create()(SortSelector)
+
+WrappedSortSelector.propTypes = {
+  field: PropTypes.string,  
+  form: PropTypes.object
+}
+
+export default connect(mapStateToProps)(WrappedSortSelector)
