d4e15e6db673627c3c42acb864335667991d4702
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-24 22:59:50 -0300

    Action responsible for receiving a collection of comments

--- frontend/src/actions/comments.js
+++ frontend/src/actions/comments.js
@@ -1,51 +1,103 @@
-import commentApi from '../utils/CommentApi'
+import commentApi from '../api/CommentApi'
 import Comment from '../model/Comment'
 import { showLoading, hideLoading } from 'react-redux-loading'
 import { showError } from './error';
+import { incrementCommentCounter, decrementCommentCounter } from './posts';
+import { setSuccess } from './success';
 export const RECEIVE_COMMENTS = 'RECEIVE_COMMENTS'
 export const ADD_COMMENT = 'ADD_COMMENT'
+export const EDIT_COMMENT = 'EDIT_COMMENT'
 export const SET_VOTE_COMMENT = 'SET_VOTE_COMMENT'
 export const UP_VOTE_COMMENT = 'UP_VOTE_COMMENT'
 export const DOWN_VOTE_COMMENT = 'DOWN_VOTE_COMMENT'
-
-export function receiveComments(postId, comments){
+export const DELETE_COMMENT = 'DELETE_COMMENT'
+export const DELETE_ALL_COMMENTS = 'DELETE_ALL_COMMENTS'
+/**
+ * Action responsible for receiving a collection of comments
+ * @param {string} comments Collection of comments
+ */
+export function receiveComments(comments){
     return {
         type: RECEIVE_COMMENTS,
-        postId,
         comments
     }
 }
-
-function addComment(postId, comment){
+/**
+ * Action responsible for adding a comment
+ * @param {any} comment The comment
+ */
+function addComment(comment){
     return {
-        type: ADD_COMMENT,
-        postId,
+        type: ADD_COMMENT,        
         comment
     }
 }
 
-function setVoteComment( postId, commentId, voteScore ){
+/**
+ * Action responsible for deleting a comment
+ * @param {string} id The comment id
+ */
+function deleteComment(id){
     return {
-        type: SET_VOTE_COMMENT,
-        postId,
-        commentId,
+        type: DELETE_COMMENT,        
+        id
+    }
+}
+
+/**
+ * Action responsible for delete all comments in a post
+ * @param {string} id The comment id
+ */
+export function deleteAllComments(parentId){
+    return {
+        type: DELETE_ALL_COMMENTS,        
+        parentId
+    }
+}
+
+/**
+ * Action responsible for changing comment details
+ * @param {string} id The comment id 
+ * @param {string} body The body of the comment
+ */
+function editComment(id, body) {
+    return {
+        type: EDIT_COMMENT,
+        id,        
+        body
+    }
+}
+/**
+ * Action responsible for confirming the vote score after saving it in the database. In a multi-user 
+ * environment, the vote may change before sending.
+ * @param {string} id comment id
+ * @param {number} voteScore vote score
+ */
+function setVoteComment( id, voteScore ){
+    return {
+        type: SET_VOTE_COMMENT,        
+        id,
         voteScore
     }
 }
-
-function upVoteComment( postId, commentId ){
+/**
+ * Uptimist update - action responsible for increase the vote score before sending to database 
+ * @param {string} id The comment id
+ */
+function upVoteComment( id ){
     return {
-        type: UP_VOTE_COMMENT,
-        postId,
-        commentId
+        type: UP_VOTE_COMMENT,        
+        id
     }
 }
-
-function downVoteComment( postId, commentId ){
+/**
+ * Uptimist update - action responsible for descreasing the vote score before sending to database 
+ * @param {string} id The comment id
+ */
+function downVoteComment( id ){
     return {
-        type: DOWN_VOTE_COMMENT,
-        postId,
-        commentId        
+        type: DOWN_VOTE_COMMENT,        
+        id        
     }
 }
 
@@ -55,7 +107,7 @@ export function handleAllComments(postId){
         try {
             const res = await commentApi.getAll(postId);
             const comments = res.data;
-            dispatch(receiveComments(postId, comments));
+            dispatch(receiveComments(comments));
         }
         catch (e) {
             dispatch(showError('New comment', 'The was an error fetching comments. Try again.'));
@@ -71,9 +123,32 @@ export function handleAddComment(postId, body, author){
         const newComment = new Comment(body, author, postId)
 
         try {
+            dispatch(incrementCommentCounter(postId))
             const res = await commentApi.add(newComment);
             const comment = res.data;
-            dispatch(addComment(postId, comment));
+            dispatch(addComment(comment));
+            dispatch(setSuccess());
+        }
+        catch (e) {
+            dispatch(decrementCommentCounter(postId))
+            dispatch(showError('New comment', 'The was an error adding new comment. Try again.'));            
+        }
+        return dispatch(hideLoading);    
+    }
+}
+
+export function handleDeleteComment(postId, id){
+    return async (dispatch) => {       
+        dispatch(showLoading)      
+
+        try {
+            const res = await commentApi.delete(id);
+            const comment = res.data;
+            if (comment.deleted){
+                dispatch(deleteComment(id));
+                dispatch(decrementCommentCounter(postId))
+                dispatch(setSuccess())
+            }
         }
         catch (e) {
             dispatch(showError('New comment', 'The was an error adding new comment. Try again.'));
@@ -81,35 +156,58 @@ export function handleAddComment(postId, body, author){
         return dispatch(hideLoading);    
     }
 }
-
-export function handleUpVoteComment(postId, commentId){
+/**
+ * The handler responsible for sending changed comment details
+ * @param {string} id The id of the comment 
+ * @param {string} body the body of the comment
+ */
+export function handleEditComment(id, body) {
+    return async (dispatch) => {
+        dispatch(showLoading)
+        try {
+            await commentApi.edit(id, body)
+            dispatch(editComment(id, body))
+        } catch (e) {
+            dispatch(showError('Edit comment', 'The was an error editing comment. Try again.'));
+        }
+        return dispatch(hideLoading)
+    }
+}
+/**
+ * The async handler responsible for increase the vote score
+ * @param {string} id The comment id
+ */
+export function handleUpVoteComment(id){
     return async (dispatch) => {     
         dispatch(showLoading)
         try {
-            dispatch(upVoteComment(postId, commentId));
-            const res = await commentApi.upVote(commentId);
+            dispatch(upVoteComment(id));
+            const res = await commentApi.upVote(id);
             const comment = res.data;
-            dispatch(setVoteComment(postId, commentId, comment.voteScore));
+            dispatch(setVoteComment(id, comment.voteScore));
         }
         catch (e) {
-            dispatch(downVoteComment(postId, commentId));
+            dispatch(downVoteComment(id));
             dispatch(showError('Up vote', 'The was an error up voting the comment. Try again.'));
         }
         return dispatch(hideLoading);
     }
 }
-
-export function handleDownVoteComment(postId, commentId){
+/**
+ * The async handler responsible for decreasing the vote score
+ * @param {string} id The comment id
+ */
+export function handleDownVoteComment(id){
     return async (dispatch) => {     
         dispatch(showLoading)
         try {
-            dispatch(downVoteComment(postId, commentId));
-            const res = await commentApi.downVote(commentId);
+            dispatch(downVoteComment(id));
+            const res = await commentApi.downVote(id);
             const comment = res.data;
-            dispatch(setVoteComment(postId, commentId, comment.voteScore));
+            dispatch(setVoteComment(id, comment.voteScore));
         }
         catch (e) {
-            dispatch(upVoteComment(postId, commentId));
+            dispatch(upVoteComment(id));
             dispatch(showError('Down vote', 'The was an error down voting the comment. Try again.'));
         }
         return dispatch(hideLoading);
