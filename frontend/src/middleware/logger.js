683a0453c98a244d592c20766aeeb9a81b4a1307
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-24 23:19:11 -0300

    Middleware responsible for logging action details and status details in the browser console. Available only in the development environment.

--- frontend/src/middleware/logger.js
+++ frontend/src/middleware/logger.js
@@ -1,10 +1,19 @@
 
+const enabled = process.env.NODE_ENV === 'development'
+/**
+ * Middleware responsible for logging action details and status details in the browser console. Available only in the development environment.
+ */
 const logger = (store) => (next) => (action) => {
-    console.group(action.type)
-        console.log('The action:', JSON.stringify(action))
-        const returnValue = next(action)
-        console.log('The new state:', JSON.stringify(store.getState()))
-    console.groupEnd()
+    let returnValue
+    if (enabled){
+        console.group(action.type)
+            console.log('The action:', JSON.stringify(action))
+            returnValue = next(action)
+            console.log('The new state:', JSON.stringify(store.getState()))
+        console.groupEnd()
+    }else{
+        returnValue = next(action)
+    }
    
     return returnValue
 }
