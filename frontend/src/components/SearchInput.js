fa4196c3250d4c9db8f9ac7205210359f99181ed
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2019-02-24 23:25:39 -0300

    Component responsible for creating a search field based on the Search component from React UI library ANTD

---
+++ frontend/src/components/SearchInput.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+import { Input, Form } from 'antd';
+import { connect } from 'react-redux'
+import { handleSetSearch } from '../actions/search';
+/**
+ * Component responsible for creating a search field based on the Search component from React UI library ANTD
+ */
+class SearchInput extends Component{
+  
+    /**
+     * Call routine parent responsible for searching posts and comments
+     * @param {string} field field name for sorting
+     */                  
+    handleSearch(value){      
+        this.props.dispatch(handleSetSearch(value))
+    }
+           
+    render(){        
+      const { getFieldDecorator } = this.props.form;
+      const Search = Input.Search;
+      const formItemLayout = {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 10 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 14 },
+        },
+      };
+      return  ( <Form id="search"><Form.Item  {...formItemLayout} style={{marginBottom:0}}>{
+        getFieldDecorator('sort-selector', 
+        {          
+          initialValue: ''
+        })(          
+            <Search
+                placeholder="input search text"
+                onSearch={value => this.handleSearch(value)}
+                enterButton
+                />
+      )
+      }</Form.Item></Form>)
+    }
+}
+
+const WrappedSearchInput = Form.create()(SearchInput)
+
+export default connect()(WrappedSearchInput)
